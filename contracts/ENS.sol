d41e63287a20b4c8c6cb7599e09099cdfc8a6c87
Bytangle <dev.bytangle@gmail.com>
2022-09-16 07:42:35 +0100

    Add ENS.sol

---
+++ contracts/ENS.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >= 0.8.0 < 0.9.0;
+
+/// @title ENS - Ethereum Name Service
+abstract contract ENS {
+    /// @dev get resolver
+    /// @param _node node hash
+    /// @return resolver contract
+    function resolver(bytes32 _node) public virtual view returns (Resolver);
+}
+
+/// @title Resolver
+abstract contract Resolver {
+    /// @dev get resolver address
+    /// @param _node node hash
+    /// @return address of resolver
+    function addr(bytes32 _node) public virtual view returns (address);
+}
